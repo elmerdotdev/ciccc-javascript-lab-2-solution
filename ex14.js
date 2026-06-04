127ee061cfc04170d67b464ffdc5a98abadca815
Elmer Balbin <contact@elmer.dev>
2026-06-04 05:07:08 -0700

    update ex14.js

--- ex14.js
+++ ex14.js
@@ -1,8 +1,21 @@
-// Exercise 14: Write a closure function `createCart` that returns an object with two methods: 
+// Exercise 14: Write a closure function `createCart` that returns an object with two methods:
 // `addItem(item)` to add an item to the cart, and `getItems()` to retrieve the list of added items.
 // Example: const cart = createCart(); cart.addItem("apple"); cart.getItems(); // ["apple"]
 
+function createCart() {
+  const items = [];
+
+  return {
+    addItem: function (newItem) {
+      items.push(newItem);
+    },
+    getItems: function () {
+      return items;
+    },
+  };
+}
+
 const cart = createCart();
-cart.addItem("apple");
-cart.addItem("banana");
+cart.addItem('apple');
+cart.addItem('banana');
 console.log(cart.getItems()); // Expected output: ["apple", "banana"]
